8d94d94fb50924cdd6b39e9a5ec1d9893905387d
Erick Bauman <erick.bauman@utdallas.edu>
2019-02-14 18:41:03 -0600

    Added 64-bit compilation support, minor cleanup of test binary
    Now the library can be built as 32-bit or 64-bit, and I have added
    error warning flags to compilation.

--- test.c
+++ test.c
@@ -1,25 +1,26 @@
 #include "pagealloc.h"
 #include <stdio.h>
 
-int main(int argc, char** argv){
+int main(){
 	pa_entry_t pa_buf;
-	uint8_t* buf;
+	char* buf;
 	int i;
 	if( page_alloc(&pa_buf, 27) ){
-		buf = (uint8_t*)pa_buf.address;
+		buf = (char*)pa_buf.address;
 		for( i = 0; i < 26; i++ ){
 			buf[i] = 'A'+i;
 		}
 		puts( buf );
 		if( page_realloc(&pa_buf, 4096+27) ){
-			buf = (uint8_t*)(pa_buf.address + 4096);
+			buf = (char*)(pa_buf.address + 4096);
 			for( i = 0; i < 26; i++ ){
 				buf[i] = 'a'+i;
 			}
-			puts(buf);
-			buf = (uint8_t*)(pa_buf.address);
-			puts(buf);
+			puts( buf );
+			buf = (char*)(pa_buf.address);
+			puts( buf );
 			page_free(&pa_buf);		
 		}
 	}
+	return 0;
 }
